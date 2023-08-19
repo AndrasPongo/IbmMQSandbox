c3fbdbac624d9a51f8db2c628774e96bb98c5795
AndrasPongo <andras.pongo14@gmail.com>
2023-08-19 16:14:08 +0200

    Initial commit

---
+++ src/main/java/com/andrelcode/ibmmqsandbox/IbmMqSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.andrelcode.ibmmqsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmMqSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmMqSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrelcode/ibmmqsandbox/IbmMqSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelcode.ibmmqsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmMqSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
